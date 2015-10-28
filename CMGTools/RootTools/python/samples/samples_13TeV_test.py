4ae242763ae6302c23a3965b773e3c75af2f2891
Johannes <johannes.brandstetter@cern.ch>
2015-10-28 17:31:50 +0100

    new MC2 samples

--- CMGTools/RootTools/python/samples/samples_13TeV_test.py
+++ CMGTools/RootTools/python/samples/samples_13TeV_test.py
@@ -14,10 +14,19 @@ WJetsToLNu_TuneCUETP8M1 = kreator.makeComponentHEPHY("WJetsToLNu_TuneCUETP8M1","
 WJetsToLNu_TuneCUETP8M1_2 = kreator.makeComponentHEPHY("WJetsToLNu_TuneCUETP8M1_2","/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/jbrandst-Wjets_MC2-a521298c1f16775ea1cf7ade8c6f81c7/USER","PRIVATE",".*root","phys03",1.0)
 
 QCD_Pt15TTo7000_TuneZ2starFlat = kreator.makeComponentHEPHY("QCD_Pt15TTo7000_TuneZ2starFlat","/QCD_Pt-15TTo7000_TuneZ2star-Flat_13TeV_pythia6/jbrandst-Spring15_QCD_Asympt25ns_2209-944f06e45b67adf54f7518dd64f83326/USER","PRIVATE",".*root","phys03",1.0)
+
 TT_TuneCUETP8M1 = kreator.makeComponentHEPHY("TT_TuneCUETP8M1","/TT_TuneCUETP8M1_13TeV-powheg-pythia8/jbrandst-Spring15_TT_Asympt25ns_2109-944f06e45b67adf54f7518dd64f83326/USER","PRIVATE","*.root","phys03",1.0)
+TT_TuneCUETP8M1_small = kreator.makeComponentHEPHY("TT_TuneCUETP8M1_small","/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/jbrandst-TTbar_MC2-a521298c1f16775ea1cf7ade8c6f81c7/USER","PRIVATE","*.root","phys03",1.0)
+
 DYJetsToLL_M50 = kreator.makeComponentHEPHY("DYJetsToLL_M50","/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/jbrandst-Spring15_DYJets_Asympt25ns_2109-944f06e45b67adf54f7518dd64f83326/USER","PRIVATE","*.root","phys03",1.0)
+
+DYJetsToLL_M50_2 = kreator.makeComponentHEPHY("DYJetsToLL_M50","/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/jbrandst-DYJetsToLL_MC2-a521298c1f16775ea1cf7ade8c6f81c7/USER","PRIVATE","*.root","phys03",1.0)
+
 Run2015D_PromptReco_v3 = kreator.makeDataComponentHEPHY("Run2015D_PromptReco_v3","/SingleMuon/jbrandst-Run2015D-PromptReco-v3-c83e672fc034baa1893ed04e7be76a51/USER", "PRIVATE", "*.root","phys03","/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON.txt")
+
 Run2015D_05Oct2015_v1 = kreator.makeDataComponentHEPHY("Run2015D_05Oct2015_v1","/SingleMuon/jbrandst-Run2015D-05Oct2015-v1-c83e672fc034baa1893ed04e7be76a51/USER", "PRIVATE", "*.root","phys03","/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON.txt")
+
+Run2015D_05Oct2015_v1_2 = kreator.makeDataComponentHEPHY("Run2015D_05Oct2015_v1","/SingleMuon/jbrandst-Run2015D-05Oct2015-v1_2-3988b50fefc4fac1ce2afd98d7c19ed6/USER", "PRIVATE", "*.root","phys03","/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions15/13TeV/Cert_246908-258159_13TeV_PromptReco_Collisions15_25ns_JSON.txt")
 
 
 
